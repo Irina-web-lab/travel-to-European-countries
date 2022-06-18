1599f1e315481b46bcea4df8912f2290716a1179
Irina-web-lab <85741318+Irina-web-lab@users.noreply.github.com>
2022-06-18 23:48:39 +0200

    Update Gallery.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,73 +1,70 @@
-import { useState } from 'react';
-import { slides } from './slides';
+import { useState } from "react";
 import './App.css';
+import { slides } from "./slides";
 
 function Gallery() {
+    const [picture, setPicture] = useState(0);
+    const {id, pic, depiction} = slides[picture];
+/*     console.log(slides[picture])
+ */
+/*     const [pictures] = useState(slides[depiction]);
+ *//*     console.log(depiction)
+ */
 
-      const [place, setPlace] = useState(0);
-      const [showText, setShowText] = useState(false);
-      /* console.log(slides[place]) */
-
-      const perviousPlace = () => {
-      setPlace((place => {
-      place --;
-      if (place < 0) {
-      return slides.length - 1;
-  }
-      return place;
-  }))
-}
-const nextPlace = () => {
-    setPlace((place => {
-      place ++;
-      if (place > slides.length - 1) {
-      place = 0;
-  } 
-      return place;
-  }))
+const perviousPicture = () => {
+    setPicture ((picture => {
+        picture --;
+        if (picture < 0) {
+            return slides.length - 1;
+        }
+        return picture;
+    }))
 }
 
-      const showTextClick = (place) => {
-                place.showMore = !place.showMore
-                setShowText(!showText);
-  }
+const nextPicture = () => {
+    setPicture ((picture => {
+        picture ++;
+        if (picture > slides.length - 1) {
+            picture = 0;
+        }
+        return picture;
+    }))
+}
 
-  return (<div>
-    <div className='gallery container'>
+const [showMore, showText, setShowText] = useState(false);
+
+const showTextClick = (item) => {
+        item.showMore = !item.showMore
+        setShowText(!showText)
+}
+
+ return (<div> 
+
+<div className='gallery container'>
                 <h2>Galleri</h2>
                 </div>
-      {place.map((place => {
-      const {id, picture, depiction, showMore} = slides[place];
-
-return (<div>
-<div key={id}>
-  
-<div className='carousel container'>
+                <div className='carousel container'>
 <div className='city container'>
                 <h2 className='city'>{id}</h2>
                 </div>
 
 <div className="container">
-                <img className='picture' src={picture} width='800px' height='500px' alt='City'/>
+                <img className='picture' src={pic} width='600px' alt='City'/>
                 </div>
 
 <div className="container">
-                <p>{showMore ? depiction  : depiction.substring(0, 230) + "..."}
-                <button onClick={() => showTextClick(id)}>{showMore ? "Show less" : "Show more"}</button>
+                <p>{showMore ? depiction : depiction.substring(0, 240) + " ..."}
+                <button className='slides btn' onClick={() => showTextClick(!showMore)}>{showMore ? 'Vis mindre' : 'Vis mer'}</button>
                 </p>
                 </div>
 
 <div className="btn-container">
-                <button className='button' onClick={perviousPlace}>TILBAKE</button>
-                <button className='button' onClick={nextPlace}>NESTE</button>
+                <button className='button' onClick={perviousPicture}>TILBAKE</button>
+                <button className='button' onClick={nextPicture}>NESTE</button>
                 </div>
             </div>
-        </div>
-    </div>)
-    }))}
-  </div>
-  )}
+        </div>)
+}
 
 
 export default Gallery;
-
